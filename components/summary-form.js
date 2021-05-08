dd1762c1cf5f92b36afa1ee19000ce7287e890ea
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2021-05-08 13:55:25 +0800

    feat: implement summary form inputs

--- components/summary-form.js
+++ components/summary-form.js
@@ -1,3 +1,36 @@
+import {
+  Heading,
+  Stack,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
+
 export default function SummaryForm() {
-  return <div>summary form toto</div>;
+  return (
+    <Stack p="5">
+      <Stack>
+        <Heading>Personal Information</Heading>
+        <FormControl id="name">
+          <FormLabel>Your name</FormLabel>
+          <Input type="text" />
+        </FormControl>
+        <FormControl id="phone">
+          <FormLabel>Your phone</FormLabel>
+          <Input type="tel" />
+        </FormControl>
+      </Stack>
+      <Stack>
+        <Heading>Delivery Information</Heading>
+      </Stack>
+      <Stack>
+        <Heading>Product Summary</Heading>
+      </Stack>
+      <Stack>
+        <Heading>Payment Summary</Heading>
+      </Stack>
+    </Stack>
+  );
 }
