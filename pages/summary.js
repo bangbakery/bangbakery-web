7e3f14ebd4c5d16b12ca926b8a113d8bb9cf1b71
M Haidar Hanif <git@mhaidarhanif.com>
2021-05-08 15:33:46 +0700

    fix: order total payment

--- pages/summary.js
+++ pages/summary.js
@@ -1,9 +1,9 @@
-import Head from "next/head";
-import Link from "next/link";
-import { Container, Heading, Flex, Button } from "@chakra-ui/react";
+import Head from 'next/head'
+import Link from 'next/link'
+import { Container, Heading, Flex, Button } from '@chakra-ui/react'
 
-import Header from "../components/header";
-import SummaryForm from "../components/summary-form";
+import Header from '../components/header'
+import SummaryForm from '../components/summary-form'
 
 export default function SummaryPage() {
   return (
@@ -24,11 +24,11 @@ export default function SummaryPage() {
           <Button as="a" width="100%" bg="#FFFF01" size="lg" rounded="full">
             <Flex justify="space-between" width="100%">
               <span>Place your order</span>
-              <span>₱450</span>
+              <span>₱480</span>
             </Flex>
           </Button>
         </Link>
       </Flex>
     </Container>
-  );
+  )
 }
