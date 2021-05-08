2234401aa1f53599edbec6d1e0f54a4bbf17577a
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2021-05-08 13:43:01 +0800

    feat: implement react summary page

---
+++ components/summary-form.js
@@ -0,0 +1,3 @@
+export default function SummaryForm() {
+  return <div>summary form toto</div>;
+}
--- pages/summary.js
+++ pages/summary.js
@@ -1,5 +1,18 @@
-import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { Container, Heading, Flex, Button } from "@chakra-ui/react";
 
-export default function summary() {
-  return <div>summay page</div>;
+import Header from "../components/header";
+import SummaryForm from "../components/summary-form";
+
+export default function SummaryPage() {
+  return (
+    <Container p="0">
+      <Head>
+        <title>Order Summary - BangBakery</title>
+      </Head>
+      <Header />
+      <SummaryForm />
+    </Container>
+  );
 }
