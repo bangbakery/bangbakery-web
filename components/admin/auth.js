d57b6fcbd59ee706bb867260dac1563172dcb52d
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2021-05-29 19:38:41 +0800

    feat: admin login form

--- components/admin/auth.js
+++ components/admin/auth.js
@@ -1,5 +1,26 @@
-import React from "react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Stack,
+  Button,
+} from "@chakra-ui/react";
 
 export default function AdminAuth() {
-  return <div>this is admin auth</div>;
+  return (
+    <Stack p="10" spacing="5">
+      <FormControl id="email">
+        <FormLabel>Email address</FormLabel>
+        <Input type="email" />
+      </FormControl>
+      <FormControl id="password">
+        <FormLabel>Password</FormLabel>
+        <Input type="password" />
+      </FormControl>
+      <Button colorScheme="teal" width="100%">
+        Login
+      </Button>
+    </Stack>
+  );
 }
