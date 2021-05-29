8a02d2598ba327066d58cd9c531e52bcecb8e23b
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2021-05-29 20:40:11 +0800

    feat: session added

--- components/admin/auth.js
+++ components/admin/auth.js
@@ -1,4 +1,5 @@
 import { supabase } from "../../lib/supabase";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 import {
@@ -17,13 +18,23 @@ export default function AdminAuth() {
     formState: { errors },
   } = useForm();
 
+  const [session, setSession] = useState(null);
+
   let handleLogin = async (data) => {
-    console.log(data);
     let { user, error } = await supabase.auth.signIn({
       email: data.email,
       password: data.password,
     });
   };
+
+  useEffect(() => {
+    const supabaseSession = supabase.auth.session();
+    setSession(supabaseSession);
+  });
+
+  if (session) {
+    return <div>you are logged in</div>;
+  }
 
   return (
     <Stack as="form" p="10" spacing="5" onSubmit={handleSubmit(handleLogin)}>
