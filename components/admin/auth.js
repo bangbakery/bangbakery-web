c0bbb918dd5b0a149ccb435b91bdb5cd74197b1b
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2021-05-29 19:55:33 +0800

    feat: login via supabase and react hook form

--- components/admin/auth.js
+++ components/admin/auth.js
@@ -1,3 +1,6 @@
+import { supabase } from "../../lib/supabase";
+import { useForm } from "react-hook-form";
+
 import {
   FormControl,
   FormLabel,
@@ -8,17 +11,31 @@ import {
 } from "@chakra-ui/react";
 
 export default function AdminAuth() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  let handleLogin = async (data) => {
+    console.log(data);
+    let { user, error } = await supabase.auth.signIn({
+      email: data.email,
+      password: data.password,
+    });
+  };
+
   return (
-    <Stack p="10" spacing="5">
+    <Stack as="form" p="10" spacing="5" onSubmit={handleSubmit(handleLogin)}>
       <FormControl id="email">
         <FormLabel>Email address</FormLabel>
-        <Input type="email" />
+        <Input type="email" {...register("email", { required: true })} />
       </FormControl>
       <FormControl id="password">
         <FormLabel>Password</FormLabel>
-        <Input type="password" />
+        <Input type="password" {...register("password", { required: true })} />
       </FormControl>
-      <Button colorScheme="teal" width="100%">
+      <Button type="submit" colorScheme="teal" width="100%">
         Login
       </Button>
     </Stack>
