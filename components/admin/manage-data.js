e89a442e7b99b0294c15d72c538905c5f41b90b9
sholost <stoedirman@gmail.com>
2021-07-04 18:06:39 +0700

    feat(admin): create and link products and orders

--- components/admin/manage-data.js
+++ components/admin/manage-data.js
@@ -1,12 +1,21 @@
 import { Button, Stack, Box, Heading, ButtonGroup } from '@chakra-ui/react'
+import Link from 'next/link'
 
 export default function AdminManageData() {
   return (
     <Stack>
       <Heading>Manage Data</Heading>
       <ButtonGroup colorScheme="teal" size="xl">
-        <Button padding={7}>Products</Button>
-        <Button padding={7}>Orders</Button>
+        <Link href="/products">
+          <Button as="a" padding={7}>
+            Products
+          </Button>
+        </Link>
+        <Link href="/orders">
+          <Button as="a" padding={7}>
+            Orders
+          </Button>
+        </Link>
       </ButtonGroup>
     </Stack>
   )
---
+++ pages/orders.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default function OrdersPage() {
+  return (
+    <Layout>
+      <p>Orders Page</p>
+    </Layout>
+  )
+}
