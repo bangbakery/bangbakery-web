dd6f866b8a5ed10cc41c1fee934e77518d9dce6d
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2021-07-20 11:48:52 -0700

    Add increase decrement product amount feature

--- components/products.js
+++ components/products.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import {
   Stack,
   Heading,
@@ -6,53 +6,72 @@ import {
   IconButton,
   HStack,
   Text,
-} from '@chakra-ui/react'
-import { MinusIcon, AddIcon } from '@chakra-ui/icons'
+} from "@chakra-ui/react";
+import { MinusIcon, AddIcon } from "@chakra-ui/icons";
 
-import { supabase } from '../lib/supabase'
-import { useSWR, fetcher } from '../lib/swr'
+import { supabase } from "../lib/supabase";
+import { useSWR, fetcher } from "../lib/swr";
 
 export default function Products() {
-  const { data, error } = useSWR('/api/products', fetcher)
+  const { data, error } = useSWR("/api/products", fetcher);
 
-  if (error) return <div>Failed to load products</div>
-  if (!data) return <div>Loading products...</div>
+  if (error) return <div>Failed to load products</div>;
+  if (!data) return <div>Loading products...</div>;
 
   return (
     <SimpleGrid columns={2} spacing={10} p={5} pb="100px">
       {data.map((product, index) => {
-        return (
-          <Stack key={product.name}>
-            <img src={product.image} alt={product.name} />
-            <Heading as="h2" size="md" fontWeight="regular">
-              {product.name}
-            </Heading>
-            <Heading as="h3" size="md" fontWeight="regular">
-              ₱{product.price}
-            </Heading>
-            <HStack>
-              <IconButton
-                aria-label="decrease amount"
-                isRound
-                size="sm"
-                colorScheme="red"
-                variant="outline"
-                icon={<MinusIcon />}
-              />
-              <span>0</span>
-              <IconButton
-                aria-label="increase amount"
-                isRound
-                size="sm"
-                colorScheme="green"
-                variant="outline"
-                icon={<AddIcon />}
-              />
-            </HStack>
-            <Text fontSize="md">{product.quantity} breads left</Text>
-          </Stack>
-        )
+        return <ProductCard product={product} />;
       })}
     </SimpleGrid>
-  )
+  );
 }
+
+const ProductCard = ({ product }) => {
+  const [amount, setAmount] = useState(0);
+
+  const decreaseAmount = () => {
+    if (amount >= 1) {
+      setAmount(amount - 1);
+    }
+  };
+
+  const increaseAmount = () => {
+    if (amount < 10) {
+      setAmount(amount + 1);
+    }
+  };
+  return (
+    <Stack key={product.name}>
+      <img src={product.image} alt={product.name} />
+      <Heading as="h2" size="md" fontWeight="regular">
+        {product.name}
+      </Heading>
+      <Heading as="h3" size="md" fontWeight="regular">
+        ₱{product.price}
+      </Heading>
+      <HStack>
+        <IconButton
+          aria-label="decrease amount"
+          isRound
+          size="sm"
+          colorScheme="red"
+          variant="outline"
+          icon={<MinusIcon />}
+          onClick={decreaseAmount}
+        />
+        <span>{amount}</span>
+        <IconButton
+          aria-label="increase amount"
+          isRound
+          size="sm"
+          colorScheme="green"
+          variant="outline"
+          icon={<AddIcon />}
+          onClick={increaseAmount}
+        />
+      </HStack>
+      <Text fontSize="md">{product.quantity} breads left</Text>
+    </Stack>
+  );
+};
